a90505a937cff1055bdf2d623d40513e5cf9b951
Ahmed Shaaban <111895280+AhmedShaban52@users.noreply.github.com>
2025-06-27 11:06:58 +0300

    11

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning>
+      <body className={inter.className} >
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         {children}
       </body>
